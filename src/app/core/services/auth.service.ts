4abcdcbebe7d264c819fe9c9b5be8e8336df82f9
vishnu-inx <vishnu.m@inheritx.com>
2023-03-30 17:20:44 +0530

    implement error-handlor for services

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,39 +1,79 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { StorageService } from './storage.service';
 import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { GeneralService } from './general.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-
+    protected apiURL ="http://localhost:3000";
     constructor(
         private http: HttpClient,
         private storageService: StorageService,
-        private router: Router
+        private router: Router,
+        private generalService: GeneralService
     ) { }
 
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    }
+
     login(): Observable<any> {
-        return this.http.get(`http://localhost:3000/signup`);
+        return this.http.get(`${this.apiURL}/signup`)
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        )
     }
 
     signUp(payload: any): Observable<any> {
-        return this.http.post(`http://localhost:3000/signup`, payload);
+        return this.http.post(`${this.apiURL}/signup`, payload , this.httpOptions)
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        )
     }
 
     updateProfile(payload: any, id: string): Observable<any> {
-        return this.http.put(`http://localhost:3000/signup/${id}/`, payload);
+        return this.http.put(`${this.apiURL}/signup/${id}/`, payload, this.httpOptions)
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        )
     }
 
     profile(): Observable<any> {
-        return this.http.get(`http://localhost:3000/signup`);
+        return this.http.get(`${this.apiURL}/signup`)
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        )
     }
 
     logout() {
         this.storageService.clearCookie();
         this.router.navigate(['/']);
+    }
+
+    handleError(error: any) {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+            // Get client-side error
+            errorMessage = error.error.message;
+            console.error(errorMessage);
+        } else {
+            // Get server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+            console.error(errorMessage);
+        }
+        this.generalService.displayError(errorMessage);
+        return throwError(errorMessage);
     }
 
 }
--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,28 +1,70 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { GeneralService } from './general.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
+    protected apiURL ="http://localhost:3000";
+    constructor(
+        private http: HttpClient,
+        private generalService: GeneralService
+        ) { }
 
-    constructor(private http: HttpClient) { }
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    }
 
     getAllProduct(): Observable<any> {
-        return this.http.get(`http://localhost:3000/products`);
+        return this.http.get(`${this.apiURL}/products`)
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        )
     }
 
     getProduct(id: any): Observable<any> {
-        return this.http.get(`http://localhost:3000/products/${id}`);
+        return this.http.get(`${this.apiURL}/products/${id}`)
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        )
     }
 
     searchProduct(search: any): Observable<any> {
-        return this.http.get(`http://localhost:3000/products?q=${search}`);
+        return this.http.get(`${this.apiURL}/products?q=${search}`)
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        )
     }
 
     getProductByCategory(category: any): Observable<any> {
-        return this.http.get(`http://localhost:3000/products?category=${category}`);
+        return this.http.get(`${this.apiURL}/products?category=${category}`)
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        )
+    }
+
+    handleError(error: any) {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+            // Get client-side error
+            errorMessage = error.error.message;
+            console.error(errorMessage);
+        } else {
+            // Get server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+            console.error(errorMessage);
+        }
+        this.generalService.displayError(errorMessage);
+        return throwError(errorMessage);
     }
 
 
